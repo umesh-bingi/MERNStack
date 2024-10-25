751ace08dbd4ef321a487d2b46d87c79f41c7c1d
umesh-bingi <ubingi.03@gmail.com>
2024-10-25 16:43:29 +0530

    new

--- D3/app.js
+++ D3/app.js
@@ -1,1 +1,26 @@
-console.log("Hello world");
+function myname(){
+    console.log("Umesh bingi");
+}
+function login(event){
+    event.preventDefault();
+    let mail=document.getElementById("mail").value;
+    let pass=document.getElementById("pass").value;
+    if(mail==="umesh@gmail" && pass==="1234")
+    {
+        alert("Successfully Called");
+        API();
+    }
+    else
+        alert("not done");
+}
+async function API()
+{
+    console.log("API called");
+    try{
+        const response= await fetch("https://671b71052c842d92c37fece8.mockapi.io/webworkshop/products");
+        console.log(await response.json());
+    }
+    catch(error){
+        console.error(error);
+    }
+}
